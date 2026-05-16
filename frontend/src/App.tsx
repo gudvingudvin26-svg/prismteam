a2662bd37527a21473ebccdd3e4ef624c05fe1a5
Igor Bankovsky <gudvin>
2026-05-16 12:27:33 +0300

    временно убрал PrivateRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,4 @@
-﻿import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import JoinQuiz from './pages/JoinQuiz';
-import QuizSession from './pages/QuizSession';
-import Results from './pages/Results';
+﻿import Results from './pages/Results';
 import QuizList from './pages/organizer/QuizList';
 import CreateQuiz from './pages/organizer/CreateQuiz';
 import Stats from './pages/Stats';
@@ -22,31 +14,11 @@ function App() {
         <Route path="/play/:sessionId" element={<QuizSession />} />
         <Route path="/results/:sessionId" element={<Results />} />
 
-        {/* Приватные маршруты для организатора */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/quizzes"
-          element={
-            <PrivateRoute>
-              <QuizList />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/quizzes/create"
-          element={
-            <PrivateRoute>
-              <CreateQuiz />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/quizzes" element={<QuizList />} />
+        <Route path="/quizzes/create" element={<CreateQuiz />} />
+        <Route path="/stats/:sessionId" element={<Stats />} />
+
         <Route
           path="/quizzes/:id/edit"
           element={
@@ -55,14 +27,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        <Route
-          path="/stats/:sessionId"
-          element={
-            <PrivateRoute>
-              <Stats />
-            </PrivateRoute>
-          }
-        />
+
         <Route path="*" element={<div className="p-6 text-center">404 - Страница не найдена</div>} />
       </Routes>
     </BrowserRouter>
