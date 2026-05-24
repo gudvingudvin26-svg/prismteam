b71302055fb81a32ee1687ce5d5598ee15aab3bb
Igor Bankovsky <gudvin>
2026-05-24 21:38:58 +0300

    fix: унифицирован тип _env_ во всех файлах

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,16 +1,21 @@
 ﻿/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string
+  readonly VITE_API_URL?: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
 
-interface Window {
-  _env_: {
-    API_URL?: string
-    [key: string]: string | undefined
+declare global {
+  interface Window {
+    _env_: {
+      API_URL?: string
+      VITE_API_URL?: string
+      [key: string]: string | undefined
+    }
   }
 }
+
+export {}
